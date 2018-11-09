e0f1392b11b12dcc8ada1d1389cb4273f961db1e
raa0121 <raa0121@gmail.com>
2018-11-09 18:45:00 +0900

    fix

--- web.rb
+++ web.rb
@@ -1,5 +1,6 @@
 require 'bundler'
 Bundler.require
+require 'openssl'
 require 'json'
 
 class Gh2Cd < Sinatra::Base
@@ -9,17 +10,22 @@ class Gh2Cd < Sinatra::Base
     super
   end
 
+  def verify_signature(payload_body)
+    signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['GITHUB_WEBHOOK_SEACRET'], payload_body)
+    return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
+  end
+
   get '/' do
     'hi'
   end
 
   post '/' do
+    request.body.rewind
+    payload_body = request.body.read
+    verify_signature(payload_body)
     payload = JSON.parse params[:payload], symbolize_names: true
-    if false === params[:key].empty? &&
-       params['X-Hub-Signature'] == ENV['GITHUB_WEBHOOK_SEACRET'] &&
-       'push' == params['X-GitHub-Event'] &&
-       'refs/heads/master' == payload[:ref]
-      commit_id = payload[:head]
+    if 'push' == request.env['HTTP_X_GITHUB_EVENT'] && 'refs/heads/master' == payload[:ref]
+      commit_id = payload[:after]
       data = {
         application_name: ENV['AWS_CODEDEPLOY_APPLICATION_NAME'],
         deployment_group_name: ENV['AWS_CODEDEPLOY_DEPLOYMENT_GROUP_NAME'],
